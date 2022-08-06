5dc693d717704c0b80409d0ba3becd8ca98c2c38
YURY POPOV <75533mail@gmail.com>
2022-08-06 12:04:06 -0700

    Second commit

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 import Card from './Card';
+import Button from './Button';
+import classes from './ErrorModal.module.css';
 
 const ErrorModal = props => {
-    return <Card>
-        <header>
-            <h2></h2>
-        </header>
-    </Card>;
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button>
+          Okay
+        </Button>
+      </footer>
+    </Card>
+  );
 };
 
 export default ErrorModal;
