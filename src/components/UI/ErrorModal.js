8636713b941ac519a0bc723d371bfff641c4ced7
YURY POPOV <75533mail@gmail.com>
2022-08-06 11:45:04 -0700

    First commit

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Card from './Card';
+
+const ErrorModal = props => {
+    return <Card>
+        <header>
+            <h2></h2>
+        </header>
+    </Card>;
+};
+
+export default ErrorModal;
